de2d4a3ed82e88750add9f7d6451a5330c7eaaea
Felipe <cloud.felipe@gmail.com>
2015-05-18 18:26:19 -0500

    Initial Commit

---
+++ GymApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GymApp
+//
+//  Created by Felipe Correa on 18/05/15.
+//  Copyright (c) 2015 Reyes Magos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GymApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GymApp
+//
+//  Created by Felipe Correa on 18/05/15.
+//  Copyright (c) 2015 Reyes Magos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
